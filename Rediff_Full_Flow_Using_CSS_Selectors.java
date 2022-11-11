25545ce04ad5345e98b401b17e42ad73c775ffa6
wahsan80 <w_ahsan@hotmail.com>
2022-11-11 15:48:12 -0500

    CSS Selectors, SoftAssert, WebDriverWait

--- Rediff_Full_Flow_Using_CSS_Selectors.java
+++ Rediff_Full_Flow_Using_CSS_Selectors.java
@@ -4,16 +4,21 @@ import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Rediff_Full_Flow_Using_CSS_Selectors {
 	
 	public static WebDriver driver;
+	public static SoftAssert softassert;
 	
 	@BeforeTest
 	
@@ -23,20 +28,32 @@ public class Rediff_Full_Flow_Using_CSS_Selectors {
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.rediff.com/");
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 	}
 	
 	@Test(priority=1)	
-	public void CheckURL() {
+	public void CheckUrlTitle() {
 		
-		System.out.println("Current URL is: " + driver.getCurrentUrl());
+		String CurrentUrl = driver.getCurrentUrl();
+		String CurrentTitle = driver.getTitle();
+		System.out.println("Current URL is: " + CurrentUrl);
+		System.out.println("Current Title is: " + CurrentTitle);
+		
 	}
 	
 	@Test(priority=2)
 	
 	public void SignIn() {
 		
-		driver.findElement(By.cssSelector("a.signin")).click();
+		WebElement SignInLink = driver.findElement(By.cssSelector("a.signin"));
+		softassert = new SoftAssert();
+		softassert.assertTrue(SignInLink.isEnabled());
+		
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.elementToBeClickable(SignInLink)).click();
+		
+		//driver.findElement(By.cssSelector("a.signin")).click();
 		driver.findElement(By.cssSelector("input#login1")).sendKeys("seleniumpanda@rediffmail.com");
 		driver.findElement(By.cssSelector("input#password")).sendKeys("Selenium@123");
 		driver.findElement(By.cssSelector("input.signinbtn")).click();
